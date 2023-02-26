e3c9aa0cf87871e25b4c7d54d550dacf2992d362
JonasDoesThings <jonas@jonasdoesthings.com>
2023-02-26 20:23:25 +0100

    refactor: update package metadata

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ this_directory = Path(__file__).parent
 
 setup(
     name='mkdocs-exclude-unused-files',
-    version='1.0.3',
+    version='1.0.0',
     packages=['mkdocs_exclude_unused_files'],
     url='https://github.com/JonasDoesThings/mkdocs-exclude-unused-files',
     license='MIT',
     author='Jonas Lorenz <jonas@jonasdoesthings.com>',
     author_email='jonas@jonasdoesthings.com',
-    description='A mkdocs plugin that excludes assets that are unused from being included in the final mkdocs output.',
+    description='A mkdocs plugin that excludes assets that are unused (orphaned) from being included in the final mkdocs output.',
     long_description=(this_directory / "README.md").read_text(),
     long_description_content_type='text/markdown',
     keywords=['mkdocs', 'mkdocs-plugin'],
